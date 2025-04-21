aaa408e42b771766460a203fc30c5358fc5ce22d
Kai <131925584+Kaisan5@users.noreply.github.com>
2025-04-21 07:08:30 +0530

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -14,20 +14,20 @@ from database.database import *
 
 
 async def is_subscribed1(filter, client, update):
-    if not FORCE_SUB_CHANNEL1:
-        return True
-    user_id = update.from_user.id
-    if user_id in ADMINS:
-        return True
-    try:
-        member = await client.get_chat_member(chat_id = FORCE_SUB_CHANNEL1, user_id = user_id)
-    except UserNotParticipant:
-        return False
+    if not FORCE_SUB_CHANNEL1:
+        return True
+    user_id = update.from_user.id
+    if user_id in ADMINS:
+        return True
+    try:
+        member = await client.get_chat_member(chat_id = FORCE_SUB_CHANNEL1, user_id = user_id)
+    except UserNotParticipant:
+        return False
 
-    if not member.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.MEMBER]:
-        return False
-    else:
-        return True
+    if not member.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.MEMBER]:
+        return False
+    else:
+        return True
 
 async def is_subscribed2(filter, client, update):
     if not FORCE_SUB_CHANNEL2:
